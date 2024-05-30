5253fa87014187b35a41574d6afc5a2db013beae
Ben Wu <wubin28@gmail.com>
2024-05-30 11:38:22 +0800

    feat:ch02:dicey_temperatures_rust: implement story 1 Obtain and display the sum of the two dice numbers guessed by the player

--- ch02/dicey_temperatures_rust/src/main.rs
+++ ch02/dicey_temperatures_rust/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the sum of two dice!");
+
+    println!("Please input your guess (between 2 and 12).");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
