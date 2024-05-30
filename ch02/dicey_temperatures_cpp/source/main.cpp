c158333865382cfd0c6d0019b7a1b1c71aed8928
Ben Wu <wubin28@gmail.com>
2024-05-30 15:09:51 +0800

    feat:ch02:dicey_temperatures_cpp: implement story 1 Obtain and display the sum of the two dice numbers guessed by the player

--- ch02/dicey_temperatures_cpp/source/main.cpp
+++ ch02/dicey_temperatures_cpp/source/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
 
-#include "lib.hpp"
-
 auto main() -> int
 {
-  auto const lib = library {};
-  auto const message = "Hello from " + lib.name + "!";
-  std::cout << message << '\n';
+  std::cout << "Guess the sum of two dice!" << std::endl;
+
+  std::cout << "Please input your guess (between 2 and 12): " << std::endl;
+
+  std::string guess;
+  std::getline(std::cin, guess);
+
+  std::cout << "You guessed: " << guess << std::endl;
+
   return 0;
 }
