890a37595b5563a5bf2b183a013c6a52411c9b74
Ben Wu <wubin28@gmail.com>
2024-06-01 17:04:27 +0800

    feat:ch02:dicey_temperatures_rust: implement story 2 Generate a random answer for the sum of the two dice numbers

--- ch02/dicey_temperatures_rust/src/main.rs
+++ ch02/dicey_temperatures_rust/src/main.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the sum of two dice!");
+
+    let sum_of_two_dice = rand::thread_rng().gen_range(2..=12);
+
+    println!("The sum of two dice is: {sum_of_two_dice}");
 
     println!("Please input your guess (between 2 and 12).");
 
