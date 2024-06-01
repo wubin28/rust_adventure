3c348269099a1930fdb274a8ace5753622fe81ca
Ben Wu <wubin28@gmail.com>
2024-06-01 17:37:49 +0800

    feat:ch02:dicey_temperatures_java: implement story 2 Generate a random answer for the sum of the two dice numbers

--- ch02/dicey_temperatures_java/app/src/main/java/dicey_temperatures_java/App.java
+++ ch02/dicey_temperatures_java/app/src/main/java/dicey_temperatures_java/App.java
@@ -1,10 +1,16 @@
 package dicey_temperatures_java;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
     System.out.println("Guess the sum of two dice!");
+
+    Random random = new Random();
+    int sumOfTwoDice = random.nextInt(11) + 2; // 生成 2 到 12 之间的随机数
+
+    System.out.println("The sum of two dice is: " + sumOfTwoDice);
 
     System.out.println("Please input your guess (between 2 and 12): ");
 
