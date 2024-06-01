3c4f86e284b557934224627c6ff1113c74c9f0b8
Ben Wu <wubin28@gmail.com>
2024-06-01 18:47:16 +0800

    feat:ch02:dicey_temperatures_cpp: implement story 2 Generate a random answer for the sum of the two dice numbers

--- ch02/dicey_temperatures_cpp/source/main.cpp
+++ ch02/dicey_temperatures_cpp/source/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <random>
 #include <string>
 
 auto main() -> int
 {
   std::cout << "Guess the sum of two dice!" << std::endl;
+
+  // 使用随机数引擎生成2到12之间的随机数
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(2, 12);
+
+  int sum_of_two_dice = dis(gen);
+
+  std::cout << "The sum of two dice is: " << sum_of_two_dice << std::endl;
 
   std::cout << "Please input your guess (between 2 and 12): " << std::endl;
 
