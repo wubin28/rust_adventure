20a2f3c0175d821587c3ff450cc2b0463ed04f66
Ben Wu <wubin28@gmail.com>
2024-06-02 18:33:03 +0800

    feat:ch02:dicey_temperatures_rust: start to implement story 3 Compare the answer with the sum for the first try

--- ch02/dicey_temperatures_rust/src/main.rs
+++ ch02/dicey_temperatures_rust/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -16,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&sum_of_two_dice) {
+        Ordering::Less => println!("You guessed it wrong on the first try!"),
+        Ordering::Greater => println!("You guessed it wrong on the first try"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
