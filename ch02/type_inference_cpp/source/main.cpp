575123050c273e05fcfe8dbc44c7e32263dd69a1
Ben Wu <wubin28@gmail.com>
2024-06-06 17:22:35 +0800

    feat:ch02:type_inference_cpp: print the inferred types of variables

--- ch02/type_inference_cpp/source/main.cpp
+++ ch02/type_inference_cpp/source/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <string>
+#include <typeinfo>
 
-#include "lib.hpp"
+template<typename T>
+std::string type_name(T&&)
+{
+  return typeid(T).name();
+}
 
 auto main() -> int
 {
-  auto const lib = library {};
-  auto const message = "Hello from " + lib.name + "!";
-  std::cout << message << '\n';
+  auto x = 5;
+  auto y = 3.0;
+  auto z = "Hello, World!";
+
+  std::cout << "The type of x is: " << type_name(x) << std::endl; // The type of x is: i (int)
+  std::cout << "The type of y is: " << type_name(y) << std::endl; // The type of y is: d (double)
+  std::cout << "The type of z is: " << type_name(z) << std::endl; // The type of z is: PKc (const char*)
+
   return 0;
 }
