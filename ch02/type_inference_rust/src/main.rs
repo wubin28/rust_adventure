4ea40b6d6218a8a96db709dc4931b66c59a59d0c
Ben Wu <wubin28@gmail.com>
2024-06-06 16:30:52 +0800

    feat:ch02:type_inference_rust: print the inferred types of variables

--- ch02/type_inference_rust/src/main.rs
+++ ch02/type_inference_rust/src/main.rs
@@ -1,3 +1,15 @@
+use std::any::type_name;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = 3.0;
+    let z = "Hello, World!";
+
+    println!("The type of x is: {}", type_of(x)); // The type of x is: i32
+    println!("The type of y is: {}", type_of(y)); // The type of x is: f64
+    println!("The type of z is: {}", type_of(z)); // The type of x is: &str
 }
